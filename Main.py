8c9a6503d745670a2e8e6b1b7b1fb60c4fa401df
mtbartnik <mateuszbartnik4@gmail.com>
2020-04-29 22:14:45 +0200

    added simple graphic without functionality

--- Main.py
+++ Main.py
@@ -2,18 +2,21 @@ import sys
 import pygame
 
 if __name__ == '__main__':
-    pygame.init()
 
-    size = width, height = 320, 240
-    speed = [2, 2]
-    white = 255, 255, 255
+    pygame.init()
+    size = window_width, window_height = 720, 480
 
     screen = pygame.display.set_mode(size)
+    background = pygame.image.load("resources/images/menu_background.jpg")
+    title = pygame.image.load("resources/images/title.png")
+    pygame.mixer.music.load('resources/sounds/opening.mp3')
+    pygame.mixer.music.play(-1)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill(white)
+        screen.blit(background, (0, 0))
+        screen.blit(title, (200, 25))
         pygame.display.flip()
