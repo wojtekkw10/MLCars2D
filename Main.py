3893aa73995123e857a037441b44caf343fded3f
mtbartnik <mateuszbartnik4@gmail.com>
2020-04-30 15:33:29 +0200

    added interactable button, created basic of simple menu

--- Main.py
+++ Main.py
@@ -1,16 +1,23 @@
 import sys
 import pygame
+from gameObjects.Button import Button
 
 if __name__ == '__main__':
 
     pygame.init()
-    size = window_width, window_height = 720, 480
+    size = window_width, window_height = 1200, 600
 
     screen = pygame.display.set_mode(size)
     background = pygame.image.load("resources/images/menu_background.jpg")
+    background = pygame.transform.scale(background, (window_width, window_height))
     title = pygame.image.load("resources/images/title.png")
     pygame.mixer.music.load('resources/sounds/opening.mp3')
     pygame.mixer.music.play(-1)
+    buttons_bg = 211, 211, 211
+    buttons_width, buttons_height = 200, 65
+
+    play_button = Button(buttons_bg, (window_width - buttons_width) / 2, title.get_height() * 2,
+                         buttons_width, buttons_height, "Play")
 
     while True:
         for event in pygame.event.get():
@@ -18,5 +25,12 @@ if __name__ == '__main__':
                 sys.exit()
 
         screen.blit(background, (0, 0))
-        screen.blit(title, (200, 25))
+        screen.blit(title, ((window_width - title.get_width()) / 2, title.get_height() / 2))
+        play_button.draw(screen)
+
+        if play_button.is_over(pygame.mouse.get_pos()):
+            play_button.color = 255, 255, 255
+        else:
+            play_button.color = buttons_bg
+
         pygame.display.flip()
