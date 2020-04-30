65430bb9455436bb6cf5aef1d2027de5db25dfaa
Jakub <jakub.i816@gmail.com>
2020-04-30 13:27:44 +0200

    added simple graphics

--- Main.py
+++ Main.py
@@ -1,34 +1,54 @@
 import sys
 import pygame
+import math
 
 from handlers.keyboardEventHandler import KeyboardEventHandler
 from gameObjects.car import Car
-
+from gameObjects.track import Track
 
 # def pixel(surface, color, pos):
 #    surface.fill(COLOR, (pos, (1, 1))
 
 
 if __name__ == '__main__':
+
     pygame.init()
 
-    size = width, height = 320, 240
+    fps = 200
+    clock = pygame.time.Clock()
+
+    size = width, height = 1200, 600
     speed = [2, 2]
     white = 255, 255, 255
-    car = Car(50, 50)
+    grey = (56, 59, 56)
 
     screen = pygame.display.set_mode(size)
     keyboardEvents = KeyboardEventHandler()
+
+    track = Track(screen)
+    car = Car(50, 60, screen)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                pass
             keyboardEvents.process(event)
 
         car.handle_keyboard(keyboardEvents)
         car.update()
-        screen.fill(white)
+
+        screen.fill(grey)
+
+
+        car_position_x, car_position_y = int(car.position_x), int(car.position_y)
+        if car.detect_collision(track.grid):
+            print("Collision")
+
+        track.draw_track()
         car.draw(screen)
 
         pygame.display.flip()
+        clock.tick(fps)
