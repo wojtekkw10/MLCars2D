800b73ee624ea44e8d52d75d3f7749ec56cf917f
Paweł Dychus <44323692+Ariello05@users.noreply.github.com>
2020-05-06 21:33:29 +0200

    break and turn on car physics

--- gameObjects/car.py
+++ gameObjects/car.py
@@ -17,7 +17,9 @@ def mapped_key(key):
 
 class Car:
     DIVIDER_SPEED = 0.0001
-    DIVIDER_ANGLE = 0.1
+    DIVIDER_BREAK = 0.0001
+    DIVIDER_ANGLE_INPUT = 0.01
+    DIVIDER_ANGLE = 0.05
 
     def __init__(self, x, y, screen):
         super().__init__()
@@ -33,26 +35,61 @@ class Car:
         self.angle = Angle(0)  # 0 is E, 90 is N, 180 is W, 270 is S
         self.car_points = {}
         self.rotate_car_points()
-        self.max_speed = 0.03
+        self.max_speed = 0.1
+        self.braking = 0.0  # axis [0.0,1.0]
+        self.turn = 0.0  # axis [-1.0,1.0]
 
     def handle_keyboard(self, keyboardEvents):
         if(keyboardEvents.isPressed(mapped_key('w'))):
+            self.braking -= 0.01
+            if self.braking < 0.0:
+                self.braking = 0.0
+
             self.speed += 1.0 * Car.DIVIDER_SPEED
             if self.speed > self.max_speed:
                 self.speed = self.max_speed
         elif (keyboardEvents.isPressed(mapped_key('s'))):
-            self.speed -= 1.0 * Car.DIVIDER_SPEED
-            if self.speed < -self.max_speed:
-                self.speed = -self.max_speed
+            self.braking += 0.01
+            if self.braking > 1.0:
+                self.braking = 1.0
         elif (keyboardEvents.isPressed(mapped_key('space'))):
             self.speed = 0.0
+            self.braking = 1.0
         if (keyboardEvents.isPressed(mapped_key('a'))):
-            self.angle.degree += 1.0 * Car.DIVIDER_ANGLE
-        if (keyboardEvents.isPressed(mapped_key('d'))):
-            self.angle.degree -= 1.0 * Car.DIVIDER_ANGLE
+            self.turn -= 0.01
+            if self.turn < -1.0:
+                self.turn = -1.0
+
+        elif (keyboardEvents.isPressed(mapped_key('d'))):
+            self.turn += 0.01
+            if self.turn > 1.0:
+                self.turn = 1.0
+        else:
+            self.turn *= 0.1
 
     def update(self):
-        nx = cos(self.angle.radians) * self.speed
+        if self.braking > 0.0:
+            inital = self.speed
+
+            if inital > 0.0:
+                self.speed -= self.braking * Car.DIVIDER_BREAK
+                if self.speed < 0.0:
+                    self.speed = 0.0
+            elif inital < 0.0:
+                self.speed += self.braking * Car.DIVIDER_BREAK
+                if self.speed > 0.0:
+                    self.speed = 0.0
+
+        if self.turn != 0.0:
+            if self.speed + 0.01 > self.max_speed:
+                speed_var = 1.0
+            else:
+                speed_var = (self.speed + 0.01) / self.max_speed
+
+            self.angle.degree -= 1.0 * Car.DIVIDER_ANGLE * \
+                (1.0 - self.braking) * self.turn * speed_var
+
+        nx = cos(self.angle.radians) * self. speed
         ny = sin(self.angle.radians) * self.speed
 
         self.position_x += nx
@@ -64,11 +101,11 @@ class Car:
 
         rotated = pygame.transform.rotate(self.image, self.angle.degree)
         rect = rotated.get_rect()
-        #position = pygame.Vector2(self.position_x, self.position_y)
-        #self.screen.blit(rotated, position - (rect.width/2.0, rect.height/2.0))
-        (x,y,w,h) = camera.apply(self)
+        # position = pygame.Vector2(self.position_x, self.position_y)
+        # self.screen.blit(rotated, position - (rect.width/2.0, rect.height/2.0))
+        (x, y, w, h) = camera.apply(self)
 
-        self.screen.blit(rotated, pygame.Vector2(x-w/2,y-h/2))
+        self.screen.blit(rotated, pygame.Vector2(x-w/2, y-h/2))
 
         self.rotate_car_points()
 
@@ -136,7 +173,7 @@ class Car:
                 self.speed = 0
                 self.position_x += 2
             self.update()
-            #self.draw(None, None)
+            # self.draw(None, None)
 
             if grid[x, y] == 1:
                 return True
