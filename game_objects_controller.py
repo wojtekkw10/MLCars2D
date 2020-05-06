180f89d5fbec4dbc4ad32691f29947bd6ba196ef
Paweł Dychus <44323692+Ariello05@users.noreply.github.com>
2020-05-06 21:41:22 +0200

    implement 'complex' method for camera

--- game_objects_controller.py
+++ game_objects_controller.py
@@ -19,8 +19,22 @@ class GameObjectsController:
             _, _, w, h = camera      # w = width, h = height
             return pygame.Rect(-l+window_width/2, -t+window_height/2, w, h)
 
+        def complex_camera(camera, target_rect):
+            # we want to center target_rect
+            x = -target_rect.center[0] + window_width/2
+            y = -target_rect.center[1] + window_height/2
+            # move the camera. Let's use some vectors so we can easily substract/multiply
+            # add some smoothness coolnes
+            camera.topleft += (pygame.Vector2((x, y)) -
+                               pygame.Vector2(camera.topleft)) * 0.06
+            # set max/min x/y so we don't see stuff outside the world
+            camera.x = max(-(camera.width-window_width), min(0, camera.x))
+            camera.y = max(-(camera.height-window_height), min(0, camera.y))
+
+            return camera
+
         self.camera = Camera(
-            simple_camera, self.window_width, self.window_height)
+            complex_camera, self.window_width+100, self.window_height+100)  # insert track size here
 
     # for adjusting menu in the future
     def display_menu(self):
