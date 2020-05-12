c53f44888e3541fe536e2b774d691d0868fed38d
patrxon <patrxon3@o2.pl>
2020-05-12 22:15:52 +0200

    Added get/set neural network as weight array.

--- neural_network.py
+++ neural_network.py
@@ -11,10 +11,15 @@ def generate_layer(height, width):
     return 2 * np.random.random((height, width)) - 1
 
 
-class brain:
+def flatten(matrix):
+    return np.reshape(matrix, (1, -1))[0]
+
+
+class CarNeuralNetwork:
 
     def __init__(self, id, shape_matrix):
         self.id = id
+        self.shape_matrix = shape_matrix
         self.value = 0
         self.joint_matrix = []
         self.weight_matrix = []
@@ -23,7 +28,7 @@ class brain:
             height = shape_matrix[i]
             width = shape_matrix[i + 1]
             self.joint_matrix.append(generate_layer(height, width))
-            self.weight_matrix.append(generate_layer(1, height))
+            self.weight_matrix.append(generate_layer(1, height)[0])
 
     def randomize(self):
         for i in range(len(self.joint_matrix)):
@@ -32,25 +37,39 @@ class brain:
 
         for i in range(len(self.weight_matrix)):
             shape_ = np.shape(self.weight_matrix[i])
-            self.weight_matrix[i] = generate_layer(shape_[0], shape_[1])
+            self.weight_matrix[i] = generate_layer(shape_[0], shape_[1])[0]
 
     def calc(self, input_):
 
         temp_layer = input_
         for i in range(len(self.joint_matrix)):
-            temp_layer += self.weight_matrix[i][0]
+            temp_layer += self.weight_matrix[i]
             temp_layer = nonlinear(np.dot(temp_layer, self.joint_matrix[i]))
 
         self.value = temp_layer
         return self.value
 
+    def set_weight_list(self, weight_arr):
+        shape = self.shape_matrix
+        for i in range(len(shape)-1):
+            self.weight_matrix[i] = np.array(weight_arr[0:shape[i]])
+            del(weight_arr[0:shape[i]])
+            for j in range(shape[i]):
+                self.joint_matrix[i][j] = np.array(weight_arr[0:shape[i+1]])
+                del (weight_arr[0:shape[i+1]])
 
-car_brain = brain(1, [3, 7, 3])
+    def get_weight_list(self):
+        weight_arr = []
 
-distances = [3, 5, 4]
+        for i in range(len(self.joint_matrix)):
+            weight_arr.extend(flatten(self.weight_matrix[i]))
+            weight_arr.extend(flatten(self.joint_matrix[i]))
 
-print(car_brain.calc(distances))
+        return weight_arr
 
-distances = [1, 2, 8]
 
-print(car_brain.calc(distances))
+car_brain = CarNeuralNetwork(1, [3, 4, 4, 2])
+distances = [0, 3, 2]
+output = car_brain.calc(distances)
+
+
